b03a7fbc4bd57e06325bc70fb793d4c59b6508fa
gundarohit <88605957+rohit8686@users.noreply.github.com>
2022-06-25 23:08:39 +0530

    fix : unused imports

--- src/frontend/pages/Post/Post.jsx
+++ src/frontend/pages/Post/Post.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-import { ToastContainer } from "react-toastify";
 import { CreatePost } from "../../components/CreatePost/CreatePost";
-import { FollowSuggestions } from "../../components/FollowSuggestions/FollowSuggestions";
 import { Posts } from "../../components/Posts/Posts";
-import { Search } from "../../components/Search/Search";
-import { Sidebar } from "../../components/Sidebar/Sidebar";
 
 export const Post = () => {
   return (
